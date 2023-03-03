e6756fee7488cc3a04d480632321ac5335d79955
mariolberto <mariocabrerac8@gmail.com>
2023-03-02 19:03:46 -0600

    este es mi primer git vehiculo

---
+++ App..java/src/App.java
@@ -0,0 +1,24 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Vehiculo!");
+
+     Carros carros = new Carros();
+     carros.setPrecio(precio:"$790,000");
+
+     carros.setMarca(marca:"Suburban");
+
+     carros.setModelo(modelo:"2020");
+
+     carros.setColor(color:"Negra");
+    
+    
+     public void imprimirCarro() {
+        System.out.println("EL CARRO CUESTA:" + this.precio);
+        System.out.println("LA MARCA DEL CARRO ES:" + this.marca);
+        System.out.println("EL MODELO DEL CARRO ES:" + this.modelo);
+        System.out.println("EL COLOR DEL CARRO ES:" + this.color);
+
+
+
+    }
+}
---
+++ Carros.java
@@ -0,0 +1,57 @@
+public class Carros {
+    
+    
+    private String  precio;
+    private Sgtring marca;
+    private String  modelo;
+    private String  color;
+
+    public String getPrecio() {
+        
+        return precio;
+
+    }  
+     public void setPrecio(String precio) {
+
+        this.precio = precio;
+
+    }
+
+
+    public String getMarca() {
+        
+        return marca;
+
+    }  
+    public void setMarca(String marca) {
+
+        this.precio = marca;
+
+    }
+
+    public String getModelo() {
+        
+        return modelo;
+
+    }  
+    public void modelo(String modelo) {
+
+        this.modelo = modelo;
+
+    }
+    
+    
+    public String getColor() {
+        
+        return color;
+
+    }  
+    public void setColor(String color) {
+
+        this.color = color;
+
+    }
+
+
+
+}
